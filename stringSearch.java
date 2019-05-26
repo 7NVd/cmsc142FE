4983d0bc243d9b85bf99fa83904ab477ecc3659f
Paul Jeremiah Naraval <31617826+7NVd@users.noreply.github.com>
2019-05-27 01:26:27 +0800

    Add files via upload

--- stringSearch.java
+++ stringSearch.java
@@ -1,3 +1,4 @@
+import java.util.LinkedList;
 
 public class stringSearch {
 	
@@ -27,54 +28,51 @@ public class stringSearch {
 		System.out.println("No match");
 		System.out.println(ctr+" comparisons");
 	}
-	private static int[] failureFunction(char[] p) {
-		int[] fail = new int[p.length];
-		int i = 1;
-		int j = 0;
-		while(i<p.length){
-			if(p[i]==p[j]){
-				fail[i]=j+1;
-				i++;
-				j++;
-			}else if(j>0) {
-				j = fail[j-1];
-			}else {
-				fail[i] = 0;
-				i++;
-			}		
-		}
-		return fail;
-	}
+	
 	public void KMP() {
-		char[] t = str.toCharArray();
-		int[] fail = failureFunction(p);// generates the failure function
-		int i = 0;
-		int j = 0;
-		int comp = 0;
-		while(i<t.length) {
-			if(t[i]==p[j]) {
-				comp++;
-				if(j== p.length -1) {
-					System.out.println("Match found at "+i+"! Total no of comparisons are: "+ comp);			
-					return;
-				}else {
-					i++;
-					j++;		
-				}
-			}else {
-				comp++;
-				if(j>0) {
-					j = fail[j-1];
-				}else {
-					i++;
-				}
-			}
-		}
-		return;
+		ctr = 0;
 	}
 	
 	public void boyerMoore() {
-		ctr = 0;
+		/*	Start of Last-occurrence Function	*/
+		LinkedList<Integer>[] alphabet = new LinkedList[255];
+		for(int i=0;i<alphabet.length;i++) {
+			alphabet[i] = new LinkedList<Integer>();
+			alphabet[i].add(-1);
+		}
+		for(int i=0;i<pat.length();i++) {
+			alphabet[pat.charAt(i)].addFirst(i);
+		}
+		/*	end of last-occurrence function	*/
 		
+		ctr = 0;
+		int i = pat.length()-1;
+		int j = pat.length()-1;
+		while(i<text.length()) {
+			ctr++;
+			System.out.println(i);
+			if(text.charAt(i)==pat.charAt(j)) {
+				if(j==0) {
+					System.out.println("Found at "+i);
+					System.out.println(ctr+" comparisons");
+					return;
+				}else {
+					i--;
+					j--;
+				}
+			}else {
+				int tmp=0;
+				int l;
+				do {
+					l=alphabet[(int) text.charAt(i)].get(tmp);
+					tmp++;
+				}while(l>j);
+				
+				i = i + pat.length() - l - 1;
+				j = pat.length()-1;
+			}
+		}
+		System.out.println("No match");
+		System.out.println(ctr+" comparisons");
 	}
 }
